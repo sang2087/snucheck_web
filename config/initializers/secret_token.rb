e74c58769239d4512518896bc7ae2364d0eeac64
susang <susang@gmail.com>
2013-05-12 22:46:04 +0900

    not start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snucheck::Application.config.secret_token = '29540a5382ff1549f76ccfee9c8c05a898fcd15b38058c122b2c1b089f491278091842601a0bc81c594d7596019f0e6c55be6ac50d5a1b4798baf304fe08e278'
